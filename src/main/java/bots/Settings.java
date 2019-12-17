fcbeb0fb461da8a29018c24b38c5d8d136d741a8
Ben van der Heiden <b.vdh@protonmail.com>
2019-12-17 19:37:29 +0100

    Change structure and enable use of .env files

---
+++ src/main/java/bots/Settings.java
@@ -0,0 +1,49 @@
+package bots;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Settings {
+    private String token;
+    private String commandPrefix;
+    private String activity;
+    private String botName;
+
+    public Settings(Dotenv dotenv) {
+        this.token = dotenv.get("BOT_TOKEN");
+        this.commandPrefix = dotenv.get("BOT_COMMAND_PREFIX");
+        this.activity = dotenv.get("BOT_ACTIVITY");
+        this.botName = dotenv.get("BOT_NAME");
+    }
+
+    public String getToken() {
+        if (this.token == null) {
+            return "token";
+        }
+
+        return token;
+    }
+
+    public String getCommandPrefix() {
+        if (this.token == null) {
+            return "!bot";
+        }
+
+        return commandPrefix;
+    }
+
+    public String getActivity() {
+        if (this.token == null) {
+            return "Botting";
+        }
+
+        return activity;
+    }
+
+    public String getBotName() {
+        if (this.token == null) {
+            return "CyberBot";
+        }
+
+        return botName;
+    }
+}
---
+++ src/main/java/bots/manker/MankerBot.java
@@ -0,0 +1,28 @@
+package bots.manker;
+
+import bots.Bot;
+import bots.Command;
+import bots.Settings;
+import bots.manker.commands.*;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class MankerBot extends Bot {
+    public MankerBot(Settings settings) {
+        super(settings);
+
+        this.commandList.add(new FourTwentyCommand());
+        this.commandList.add(new HelpCommand());
+        this.commandList.add(new MemeCommand());
+        this.commandList.add(new SearchCommand());
+        this.commandList.add(new YeetCommand());
+
+        for (Command command : commandList) {
+            command.setCommandPrefix(settings.getCommandPrefix());
+        }
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        super.onMessageReceived(event);
+    }
+}
